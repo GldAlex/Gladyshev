9a50703cfa9f670d70168a71fcb7db921317dda4
GldAlex <true4@inbox.ru>
2017-10-17 21:32:08 +0700

    решил задания с 1 по 4.

---
+++ LAB1/zadanie3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie3
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String a;
+            a = Console.ReadLine();
+            Console.Write(a);
+            Console.Write(" нас не остановят");
+        }
+    }
+}
