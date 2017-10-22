4f2202efb9b0e3a28b8c96c28a21bebfa7aa6e3f
GldAlex <true4@inbox.ru>
2017-10-22 19:20:10 +0600

    лаба 2

---
+++ ZADANIE4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZADANIE4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {String a;
+         a = Console.ReadLine();
+            Console.Write("Программирование это ");
+            Console.Write(a);
+        Console.Write("!");
+        }
+    }
+}
