3cccc7d2aaac395b1f7ee0ae12b39d2a7ecc452c
GldAlex <true4@inbox.ru>
2017-10-24 18:16:56 +0600

    лаба 2 2-5 задания

---
+++ LAB2/z1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double edge;
+            edge = Convert.ToDouble(Console.ReadLine());
+            double area;
+            if (edge > 0)
+            {
+                area = 6 * edge * edge;
+                Console.WriteLine(area);
+            }
+            else
+                Console.WriteLine("Значение edge должно быть положительным");
+
+        }
+    }
+}
---
+++ LAB2/z2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double edge;
+            edge = Convert.ToDouble(Console.ReadLine());
+            double volume;
+            if (edge > 0)
+            {
+               volume = edge * edge * edge;
+                Console.WriteLine(volume);
+            }
+            else
+                Console.WriteLine("Значение edge должно быть положительным");
+
+        }
+    }
+}
