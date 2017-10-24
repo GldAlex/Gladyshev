62d0d5ff8d2885e7dac9d5404efb450a48e2e9c2
GldAlex <true4@inbox.ru>
2017-10-24 19:59:07 +0600

    6 7 8

---
+++ LAB2/z7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double aInGRad;
+            aInGRad = Convert.ToDouble(Console.ReadLine());
+            double aInRad;
+           aInRad = aInGRad * Math.PI /180;
+                Console.WriteLine("{0:F4}",aInRad);
+          
+
+
+
+
+        }
+    }
+}
---
+++ LAB2/z8/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalInM;
+            totalInM = Convert.ToInt32(Console.ReadLine());
+            if (totalInM <= 0)
+            {
+                Console.WriteLine("Значение totalInM должно быть положительным");
+                return;
+            }
+            int partInCm;
+            partInCm = Convert.ToInt32(Console.ReadLine());
+            if (partInCm <= 0)
+            {
+                Console.WriteLine("Значение partInCm должно быть положительным");
+                return;
+            }
+            int quantity = totalInM * 100 / partInCm;
+            int deficit = totalInM * 100 % partInCm;
+            Console.WriteLine("Количество изделий {0} шт,остаток {1} см",quantity,deficit);
+
+
+
+
+        }
+    }
+}
+
