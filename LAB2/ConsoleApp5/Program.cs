61618b6c9e0e369d5167cb88e802bf8478f4b723
GldAlex <true4@inbox.ru>
2017-10-30 23:19:59 +0600

    до 16

---
+++ LAB2/ConsoleApp5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x;
+            double result;
+            x = Console.ReadLine();
+            double a;
+            a = double.Parse(x);
+            if (a >= 0)
+            {
+                a = 3*(Math.Sqrt(61-a));
+                Console.WriteLine("{0:F4}" ,a);
+            }
+            else
+            {
+                Console.WriteLine("Подкоренное выражение должно быть неотрицательным");
+            }
+        }
+    }
+}
---
+++ LAB2/z15.1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace z15._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double tempInC;
+            tempInC = Convert.ToDouble(Console.ReadLine());
+            double tempInF;
+            if (tempInC < -273.15)
+            {
+                Console.WriteLine("Температура должна быть выше абсолютного нуля");
+            }
+            else
+            {
+                tempInF = tempInC * 1.8 + 32;
+                Console.WriteLine("{0:F4}", tempInF);  }
+        }
+    }
+}
